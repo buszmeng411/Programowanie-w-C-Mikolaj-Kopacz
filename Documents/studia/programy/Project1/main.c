529ebf19f909f69c77ef82ec4cd4584ebfe058c3
buszmeng411 <mikolajk411@gmail.com>
2025-01-12 22:29:56 +0100

    Dodanie Lab01

---
+++ Documents/studia/programy/Project1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int numer_albumu;
+
+    printf("Wprowadz numer albumu studenta: ");
+    scanf_s("%d", &numer_albumu);
+    printf("Numer albumu studenta: %d\n", numer_albumu);
+
+    return 0;
+}
